7582c13d20f19c53c02d6f0948c33ffe5ecdb235
Dain Blodorn Kim <dainblodornkim@dains-imac.lan>
2021-01-28 16:26:11 -0800

    nuxt upgrade

--- pages/index.vue
+++ pages/index.vue
@@ -7,41 +7,39 @@
         </n-link>
       </li>
     </ul>
-    <client-only>
-      <viewport-wrapper class="canvas-wrapper">
-        <v-stage
-          ref="stage"
-          :config="configKonva"
-          @dragstart="handleDragstart"
-          @dragend="handleDragend"
-        >
-          <v-layer ref="layer">
-            <v-star
-              v-for="item in list"
-              :key="item.id"
-              :config="{
-                x: item.x,
-                y: item.y,
-                rotation: item.rotation,
-                id: item.id,
-                numPoints: 10,
-                innerRadius: 10,
-                outerRadius: 150, fill: '#ff616b',
-                opacity: 0.9,
-                draggable: true,
-                scaleX: dragItemId === item.id ? item.scale * 1.2 : item.scale,
-                scaleY: dragItemId === item.id ? item.scale * 1.2 : item.scale,
-                shadowColor: '#33ff7d',
-                shadowBlur: 120,
-                shadowOffsetX: dragItemId === item.id ? 15 : 5,
-                shadowOffsetY: dragItemId === item.id ? 15 : 5,
-                shadowOpacity: 0.6
-              }"
-            ></v-star>
-          </v-layer>
-        </v-stage>
-      </viewport-wrapper>
-    </client-only>
+    <viewport-wrapper class="canvas-wrapper">
+      <v-stage
+        ref="stage"
+        :config="configKonva"
+        @dragstart="handleDragstart"
+        @dragend="handleDragend"
+      >
+        <v-layer ref="layer">
+          <v-star
+            v-for="item in list"
+            :key="item.id"
+            :config="{
+              x: item.x,
+              y: item.y,
+              rotation: item.rotation,
+              id: item.id,
+              numPoints: 10,
+              innerRadius: 10,
+              outerRadius: 150, fill: '#ff616b',
+              opacity: 0.9,
+              draggable: true,
+              scaleX: dragItemId === item.id ? item.scale * 1.2 : item.scale,
+              scaleY: dragItemId === item.id ? item.scale * 1.2 : item.scale,
+              shadowColor: '#33ff7d',
+              shadowBlur: 120,
+              shadowOffsetX: dragItemId === item.id ? 15 : 5,
+              shadowOffsetY: dragItemId === item.id ? 15 : 5,
+              shadowOpacity: 0.6
+            }"
+          ></v-star>
+        </v-layer>
+      </v-stage>
+    </viewport-wrapper>
   </div>
 </template>
 
